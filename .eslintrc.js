e98233e3698ccdda37f7caaeeb913f026b1f49f3
zhouxiaojie <zhouxiaojie@58.com>
2023-01-04 20:16:10 +0800

    add lint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: ['rollup', 'plugin:import/typescript'],
+  parserOptions: {
+    project: ['./tsconfig.eslint.json', './packages/*/tsconfig.json'],
+    tsconfigRootDir: __dirname
+  },
+  rules: {
+    // disabling sort keys for now so we can get the rest of the linting shored up
+    'sort-keys': 'off',
+    'typescript-sort-keys/interface': 'off',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off'
+  },
+  overrides: [
+    {
+      files: ['**/fixtures/**'],
+      rules: {
+        'no-console': 'off'
+      }
+    }
+  ]
+};
